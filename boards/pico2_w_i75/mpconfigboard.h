820eaf81c1ccf7110006ab0fcb5fd57fa4fdf336
Phil Howard <github@gadgetoid.com>
2025-01-30 10:34:31 +0000

    Pico2 W i72: Fix board name.

--- boards/pico2_w_i75/mpconfigboard.h
+++ boards/pico2_w_i75/mpconfigboard.h
@@ -1,5 +1,5 @@
 // Board and hardware specific configuration
-#define MICROPY_HW_BOARD_NAME                   "Interstate 75 (Pico W)"
+#define MICROPY_HW_BOARD_NAME                   "Interstate 75 (Pico2 W)"
 
 #define MICROPY_HW_FLASH_STORAGE_BYTES          (PICO_FLASH_SIZE_BYTES - (2 * 1024 * 1024))
 
