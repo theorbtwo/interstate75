e618ac1bc3dccba5c2625fc1f84f7e79d64c3daf
Phil Howard <github@gadgetoid.com>
2025-01-30 12:55:07 +0000

    i75: Remove erroneous Pico W from name.
    Fixes a break with interstate75.py which used this to detect
    which board it was running on.

--- boards/pico_i75/mpconfigboard.h
+++ boards/pico_i75/mpconfigboard.h
@@ -1,4 +1,4 @@
 // Board and hardware specific configuration// Board and hardware specific configuration
-#define MICROPY_HW_BOARD_NAME                   "Interstate 75 (Pico W)"
+#define MICROPY_HW_BOARD_NAME                   "Interstate 75"
 
 #define MICROPY_HW_FLASH_STORAGE_BYTES          (PICO_FLASH_SIZE_BYTES - (1024 * 1024))
